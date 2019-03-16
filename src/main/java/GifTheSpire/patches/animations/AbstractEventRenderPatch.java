2b8b6a30b97c2075b248bdeb279052f9d7b5d072
lobbienjonsji <butterbrot1>
2019-03-16 18:04:28 +0100

    Cleaned up some mess

--- src/main/java/GifTheSpire/patches/animations/AbstractEventRenderPatch.java
+++ src/main/java/GifTheSpire/patches/animations/AbstractEventRenderPatch.java
@@ -1,15 +1,12 @@
 package GifTheSpire.patches.animations;
 
 import GifTheSpire.LobLib;
-import GifTheSpire.util.GifAnimation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.events.AbstractImageEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.ArrayList;
 
 @SpirePatch(clz = AbstractImageEvent.class, method = "render")
 public class AbstractEventRenderPatch {
